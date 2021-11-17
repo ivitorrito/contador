901de437aa2b4377e6c380f53c70d34dbc6706b6
Taller <ivitorrito@gmail.com>
2021-11-17 13:32:27 +0100

    17/11 desde el Taller

---
+++ src/counter/Maquinas.java
@@ -0,0 +1,18 @@
+package counter;
+
+public class Maquinas {
+
+    public String Maquina;
+
+    public Maquinas(String Maquina) {
+        this.Maquina = Maquina;
+    }
+
+    public String getMaquina() {
+        return Maquina;
+    }
+
+    public void setMaquina(String Maquina) {
+        this.Maquina = Maquina;
+    }
+}
